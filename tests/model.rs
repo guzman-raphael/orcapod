a72f33ec580961ed79e609b2a42c18f9442f98d8
Thao2004 <hoangmai@usf.edu>
2026-03-21 11:58:21 -0400

    opencv and update reference checksums for deterministic test outputs

--- tests/model.rs
+++ tests/model.rs
@@ -104,7 +104,7 @@ fn pod_job_to_yaml() -> Result<()> {
 fn hash_pod_result() -> Result<()> {
     assert_eq!(
         pod_result_style(&NAMESPACE_LOOKUP_READ_ONLY)?.hash,
-        "35abb8180349bed1f3ea8c0d84e98000ec3ace904624e94e678783891a7e710e",
+        "2df336726032846259efcb0fae11e0d51c475d4a6d174245ac1a7cd18e88e598",
         "Hash didn't match."
     );
     Ok(())
@@ -123,13 +123,13 @@ fn pod_result_to_yaml() -> Result<()> {
                 location:
                   namespace: default
                   path: output/result1.jpeg
-                checksum: 5898ca5bed67147680c6489056cbf2e90074bc51d8ca2645453742580ce74b7a
+                checksum: 869701ce3e5b751bde04acdd3c4d422b0e9b3da8793dd6a007f752b6783ed8d5
               result2:
                 kind: File
                 location:
                   namespace: default
                   path: output/result2.jpeg
-                checksum: da71a1b5f8ca6ebd1edfd11df4c83078fc50d0e6a4c9b3d642ba397d81d8e883
+                checksum: 2c414b3787a99aeb31b015a8f0620b4b6770247ec9dc518845c3d11a58b88db4
             assigned_name: simple-endeavour
             status: Completed
             created: 1737922307
